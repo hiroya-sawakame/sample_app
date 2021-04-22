6ba123eebf5f93a7b96c333f6261a80b3a5e7a66
h-sawakame <h-sawakame@semo.co.jp>
2021-04-22 23:59:34 +0900

    9.8 ok

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,20 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 現在ログインしているユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id]) # ||= は変数(@current_user)がnilかfalseのときのみ値を入れることができる
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
@@ -15,6 +23,7 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
